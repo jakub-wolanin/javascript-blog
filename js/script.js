1d27b81613aa76dfeb82cec40f7dcb0817ba433e
Jakub Wolanin <wolanin.kontakt@gmail.com>
2024-03-10 16:02:03 +0100

    get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event)
@@ -22,6 +23,8 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector:', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
